83467e92bb5e0e5c26577c87cb69e1e0c2677e08
FelipeIgansi <lipeigansi@gmail.com>
2021-04-17 00:26:03 -0300

    Incicio do cap sobre classes e metodos

--- src/fundamentos/Wrapper.java
+++ src/fundamentos/Wrapper.java
@@ -8,13 +8,14 @@ public class Wrapper {
         Short s = 1000;
 
         // Integer
-        Integer i = 10000;
+        // Integer i = 10000;
         // Integer.parseInt("10000");
-        Long l = 1000000L;
+        // Long l = 1000000L;
 
         System.out.println(b.byteValue());
         System.out.println(s.toString());
 
+      
         Boolean bo = Boolean.parseBoolean("true");
         System.out.println(bo.toString().toUpperCase());
     }
--- src/fundamentos/controle/ContinueRotulado.java
+++ src/fundamentos/controle/ContinueRotulado.java
@@ -3,7 +3,7 @@ package fundamentos.controle;
 public class ContinueRotulado {
     public static void main(String[] args) {
         externo: for (int i = 0; i <= 3; i++) {
-            interno: for (int j = 0; j <= 3; j++) {
+            for (int j = 0; j <= 3; j++) {
                 if (i == 1) {
                     continue externo;
                 }
